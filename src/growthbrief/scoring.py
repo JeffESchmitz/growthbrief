b1a71a3597e240a7b269b865cce02448ec27b90c
Jeff Schmitz <jeffeschmitz@gmail.com>
2025-09-06 16:18:50 -0500

    chore: GRS wireup - add initial scoring and features stubs

---
+++ src/growthbrief/scoring.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def score_grs(df: pd.DataFrame) -> pd.DataFrame:
+    """Stub for Growth Room Score (GRS) calculation."""
+    # TODO: Implement GRS calculation
+    return df
